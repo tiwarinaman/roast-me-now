ccce0ad6d6cce5844c49550c5f986e17f709e810
Naman Kumar <namant744@gmail.com>
2025-01-05 01:31:21 +0530

    initial commit

---
+++ src/main/java/com/naman/roastmenow/RoastMeNowApplication.java
@@ -0,0 +1,13 @@
+package com.naman.roastmenow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoastMeNowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoastMeNowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/naman/roastmenow/RoastMeNowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naman.roastmenow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoastMeNowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
